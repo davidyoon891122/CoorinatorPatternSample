e2d23c90598ba82a0b33dea787d469abc7249209
Davidyoon <davidyoon@koreainvestment.com>
2023-07-17 09:54:13 +0900

    Set the coordinator
    * Set the MainCoordinator on the SceneDelegate
    * Added a Coordinator directory
    * Set the background color on the ViewController

--- CoordinatorPatternSample/Delegate/SceneDelegate.swift
+++ CoordinatorPatternSample/Delegate/SceneDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var coordinator: MainCoordinator?
 
     func scene(
         _ scene: UIScene,
@@ -18,8 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+        let navigationController = UINavigationController()
+        coordinator = MainCoordinator(navigationController: navigationController)
+        coordinator?.start()
+        
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UIViewController()
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
